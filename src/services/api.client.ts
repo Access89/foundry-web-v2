ead2234d176118311bfa115abe0d83fb35ff0586
Appiah Gyimah Maxwell <maxappiahofficial@gmail.com>
2025-09-23 03:23:17 +0000

    fix linters

--- src/services/api.client.ts
+++ src/services/api.client.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
-import { variables } from "@/utils/helper";
+// import { variables } from "@/utils/helper";
 import axios, { AxiosRequestConfig } from "axios";
 
 
