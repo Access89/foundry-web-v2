87a029314c62b2a5dcd3be90b17c2a69d5d28cb0
sylvesteranderz <77518570+sylvesteranderz@users.noreply.github.com>
2025-10-15 18:17:07 +0000

    new update

--- vite.config.ts
+++ vite.config.ts
@@ -4,37 +4,9 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
 	plugins: [react()],
-	build: {
-		rollupOptions: {
-			output: {
-				manualChunks: {
-					vendor: ['react', 'react-dom'],
-					ui: ['@nextui-org/react'],
-					animations: ['framer-motion'],
-					icons: ['@iconify/react'],
-					routing: ['react-router-dom'],
-					state: ['@reduxjs/toolkit', 'react-redux', 'redux-persist'],
-					forms: ['react-hook-form', '@hookform/resolvers', 'formik'],
-					utils: ['axios', 'react-query', 'zod']
-				}
-			}
-		},
-		chunkSizeWarningLimit: 1000,
-		target: 'esnext',
-		minify: 'terser',
-		terserOptions: {
-			compress: {
-				drop_console: true,
-				drop_debugger: true
-			}
-		}
-	},
 	resolve: {
 		alias: {
 			'@': path.resolve(__dirname, './src'),
 		},
 	},
-	optimizeDeps: {
-		include: ['react', 'react-dom', '@nextui-org/react', 'framer-motion']
-	}
 });
